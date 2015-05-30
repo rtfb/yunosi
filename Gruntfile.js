28c0fc9a13b367eec4a2a8662ff66ec4983f7942
Vytautas Šaltenis <vytas@rtfb.lt>
2015-05-30 21:15:42 +0300

    Fix grunt-coveralls plugin loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,6 +107,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-blanket-qunit');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-coveralls');
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-qunit-blanket-lcov');
