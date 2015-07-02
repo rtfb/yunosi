25454f278807888f7afbde7d87dfdd5e543edfb8
Vytautas Šaltenis <vytas@rtfb.lt>
2015-07-02 20:05:39 +0300

    Extract a bunch of asserts into a helper func

--- tests/integration.js
+++ tests/integration.js
@@ -1,5 +1,11 @@
 module("Integration");
 
+function childEquals(testDataDiv, index, expected) {
+    equal(testDataDiv.children[index].tagName, "P");
+    equal(testDataDiv.children[index].children[0].tagName, "SPAN");
+    equal(testDataDiv.children[index].children[0].innerHTML, expected);
+}
+
 test("integration test, inline test data", function() {
     chrome.runtime.sendMessage = function(rq, contentReplacer) {
         equal(rq.method, "text-for-processing");
@@ -8,13 +14,13 @@ test("integration test, inline test data", function() {
             data: rq.data,
             uiState: fillDefaults(rq.uiState, true)
         }, null, function(response) {
-            deepEqual(response, [{
+            deepEqual(response[0], {
                 "origNode": 0,
                 "replacement": [{
                     "altered": true,
                     "text": "161 kilometers"
                 }]
-            }]);
+            });
             contentReplacer(response);
         });
     }
@@ -29,8 +35,6 @@ test("integration test, inline test data", function() {
         equal(response.text, "ok");
         var td = document.getElementById("test-data");
         equal(td.childElementCount, 1);
-        equal(td.children[0].tagName, "P");
-        equal(td.children[0].children[0].tagName, "SPAN");
-        equal(td.children[0].children[0].innerHTML, "161 kilometers");
+        childEquals(td, 0, "161 kilometers");
     });
 });
